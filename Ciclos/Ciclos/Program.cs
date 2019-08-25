4e416158cee27b0a82fd93ef671b5a3da9f7b286
JOSE ERNESTO CEBADA GARCIA <jose_ecebada@hotmail.es>
2019-08-24 23:14:19 -0500

    For según el usuario.

--- Ciclos/Ciclos/Program.cs
+++ Ciclos/Ciclos/Program.cs
@@ -6,29 +6,29 @@ namespace Ciclos
     {
         static void Main(string[] args)
         {
-            int inicio = 0, final = 0, avance = 0;
+            int inicio, final, avance;
             Console.WriteLine("Indroduzca el valor donde iniciara el ciclo");
             inicio = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Indroduzca el valor donde terminara el ciclo");
+            Console.WriteLine("\nIndroduzca el valor donde terminara el ciclo");
             final = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Indroduzca el valor del avance del ciclo");
+            Console.WriteLine("\nIndroduzca el valor del avance del ciclo");
             avance = Convert.ToInt32(Console.ReadLine());
 
 
             if (inicio > final)
             {
-                Console.WriteLine(" - Ciclo segun sus condiciones - ");
+                Console.WriteLine("\n-------> Ciclo segun sus condiciones <------- \n");
                 for(int i = inicio; i >= final; i-=avance)
                 {
-                    Console.WriteLine("i = " + i);
+                    Console.WriteLine("> i = " + i );
                 }
             }
             else if (inicio < final)
             {
-                Console.WriteLine(" - Ciclo segun sus condiciones - ");
+                Console.WriteLine(" < Ciclo segun sus condiciones > ");
                 for (int i = inicio; i <= final; i+=avance)
                 {
-                    Console.WriteLine("i = " + i);
+                    Console.WriteLine("> i = " + i);
                 }
             }
             else
